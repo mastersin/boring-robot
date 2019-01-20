050b22f6c90ab92ffc15833a28bd4bfbfbfb79fc
Ivan A. Melnikov <iv@altlinux.org>
2019-01-20 20:56:18 +0400

    Encoder, light sendor

---
+++ src/light-sensor/test.c
@@ -0,0 +1,12 @@
+
+const int sensorPin = 0; //analog pin 0
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    int val = analogRead(sensorPin);
+    Serial.println(val);
+    delay(100);
+}
