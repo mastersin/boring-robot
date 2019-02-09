b936d44a9e0bd82f0f0048c82646298a3ba74eb1
Evgeny Sinelnikov <sin@altlinux.ru>
2019-02-09 18:52:19 +0400

    Update encoders test with length in mm

--- src/encoders-test/Interrupt.ino
+++ src/encoders-test/Interrupt.ino
@@ -3,6 +3,9 @@ const byte interruptPin1 = 18;
 const byte interruptPin2 = 19;
 volatile byte state = LOW;
 
+#define DIAMETER 65
+#define STEP_PER_COUNT (DIAMETER * 314159 / 32 / 100)
+
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPin1, INPUT_PULLUP);
@@ -14,14 +17,22 @@ void setup() {
 
 volatile unsigned long counter1 = 0;
 unsigned long last_counter1 = counter1;
-unsigned long diff_counter1 = counter1;
+unsigned long diff_counter1 = 0;
 volatile unsigned long counter2 = 0;
 unsigned long last_counter2 = counter2;
-unsigned long diff_counter2 = counter2;
+unsigned long diff_counter2 = 0;
+
+unsigned long velocity_counter1 = 0;
+unsigned long velocity_counter2 = 0;
+unsigned long last_millis = 0;
 
 unsigned long velocity1 = 0;
 unsigned long velocity2 = 0;
-unsigned long last_millis = 0;
+
+inline unsigned long counter_to_length (unsigned long counter)
+{
+  return (counter * STEP_PER_COUNT) / 1000;
+}
 
 void loop() {
   unsigned long new_millis = millis();
@@ -30,25 +41,40 @@ void loop() {
     last_counter1 = counter1;
     last_counter2 = counter2;
 
-    Serial.print(counter1);
-    Serial.print(" - ");
+    unsigned long length1 = counter_to_length (last_counter1);
+    unsigned long length2 = counter_to_length (last_counter2);
+
+    Serial.print(last_counter1);
+    Serial.print(" (");
+    Serial.print(length1);
+    Serial.print(") - ");
+    Serial.print(velocity_counter1);
+    Serial.print(" (");
     Serial.print(velocity1);
-    Serial.print(" : ");
-    Serial.print(counter2);
-    Serial.print(" - ");
-    Serial.println(velocity2);
+    Serial.print(") : ");
+    Serial.print(last_counter2);
+    Serial.print(" (");
+    Serial.print(length2);
+    Serial.print(") - ");
+    Serial.print(velocity_counter2);
+    Serial.print(" (");
+    Serial.print(velocity2);
+    Serial.println(")");
     
     state = !state;
     digitalWrite(ledPin, state);
   }
 
-  if (new_millis - last_millis > 100)
+  if (new_millis - last_millis > 50)
   {
     unsigned long diff1 = last_counter1 - diff_counter1;
     unsigned long diff2 = last_counter2 - diff_counter2;
 
-    velocity1 = diff1;
-    velocity2 = diff2;
+    velocity1 = counter_to_length(velocity_counter1 + velocity_counter1) / 2;
+    velocity2 = counter_to_length(velocity_counter2 + velocity_counter2) / 2;
+
+    velocity_counter1 = diff1;
+    velocity_counter2 = diff2;
     
     last_millis = new_millis;
     diff_counter1 = last_counter1;
